f082bb67e4618093f22190bca2ad7d7ec8ffaa8f
Testus <o.semir@ostfalia.de>
2024-10-08 13:16:48 +0300

    Add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@ts-morph"],
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+});
+
+export default withBundleAnalyzer(nextConfig);
