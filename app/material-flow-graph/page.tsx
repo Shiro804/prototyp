257b7438a5150d39e4ce2faf3df63f8670fbdcee
Shiro804 <selmo1804@gmail.com>
2025-01-29 05:44:03 +0100

    Updated changed path.

--- app/material-flow-graph/page.tsx
+++ app/material-flow-graph/page.tsx
@@ -1,6 +1,6 @@
 // pages/material-flow.tsx (example)
 "use client";
-import { MaterialFlowGraphWithProvider } from "@/components/material-flow-graph-overview/MaterialFlowGraph";
+import { MaterialFlowGraphWithProvider } from "@/components/simulation-overview/MaterialFlowGraph";
 import React from "react";
 
 export default function MaterialFlowPage() {
