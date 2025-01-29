bf58ad3c8e05cf03fc37a102b713b28fed896997
Shiro804 <selmo1804@gmail.com>
2025-01-29 05:42:45 +0100

    Extracted often used variables into the theme.ts.

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,6 +1,23 @@
 "use client";
 
-import { createTheme, darken, Paper, Title } from "@mantine/core";
+import {
+  Button,
+  createTheme,
+  darken,
+  lighten,
+  Paper,
+  Title,
+} from "@mantine/core";
+
+export const BG_COLOR = "rgb(40, 37, 45)";
+export const BUTTON_ACTIVE_COLOR = "rgb(209, 212, 255)";
+export const PRIMARY = "#4d93ff";
+export const SECONDARY = "#81b2ff";
+export const PROCESSSTEP_COLOR = "#5300e8";
+export const TABLE_HEADER_COLOR = "rgb(113, 119, 221)";
+export const PENDING = "#9B59B6";
+export const COMPLETED = "#2ECC40";
+export const ACTIVE_BUTTON_COLOR = lighten(PRIMARY, 0.9);
 
 export const theme = createTheme({
   components: {
@@ -13,10 +30,34 @@ export const theme = createTheme({
     }),
     Paper: Paper.extend({
       styles: (prev) => ({
-        root: {
-          backgroundColor: darken(prev.white, 0.1),
-        },
+        root: {},
       }),
     }),
+  },
+  colors: {
+    primary: [
+      PRIMARY,
+      SECONDARY,
+      BUTTON_ACTIVE_COLOR,
+      "#94a8d0",
+      "#748dc1",
+      "#5f7cb8",
+      "#5474b4",
+      "#44639f",
+      "#39588f",
+      "#2d4b81",
+    ],
+    button: [
+      PRIMARY,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+      BUTTON_ACTIVE_COLOR,
+    ],
   },
 });
